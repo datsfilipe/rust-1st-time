ec98079ee858bd2958d415676ddb569916ebdffb
datsfilipe <datsfilipe.foss@proton.me>
2023-05-07 17:54:31 -0300

    can't mutate imutable variable (learning variables assignment)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let first_var: i32;
+
+    {
+        let second_var = 1;
+        println!("second_var: {}", second_var);
+    }
+
+    first_var = 2;
+
+    println!("first_var: {}", first_var);
+
+    first_var = 3;
+
+    return;
 }
