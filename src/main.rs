5c63a81723764b62eb5215e321f187aea97ee7eb
datsfilipe <datsfilipe.foss@proton.me>
2023-05-07 18:00:03 -0300

    feat: shadowing and adding explanation in comments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 fn main() {
+    vars();
+}
+
+fn vars() {
+    // mutable variable
     let mut first_var: i32;
 
+    // this is called scoping and is valid
     {
+        // immutable variable
         let second_var = 1;
         println!("second_var: {}", second_var);
     }
+    // this is not valid because second_var is out of scope
+    // println!("second_var: {}", second_var);
 
+    // valid because first_var is mutable
     first_var = 2;
 
     println!("first_var: {}", first_var);
 
     first_var = 3;
 
-    return;
+    // shadowing
+    let first_var = first_var + 1;
+    // the old value is still available, but can't be used anymore
+
+    println!("first_var: {}", first_var);
 }
