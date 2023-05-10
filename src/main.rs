f8aebc0cfbfc84586f6e1c62027554d7aa029b9f
datsfilipe <datsfilipe.foss@proton.me>
2023-05-09 21:14:11 -0300

    feat: tuples, enums, structs, debug instructions and syntax to print values with println!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+// the #[derive(Debug)] syntax lets us see certain values during the code execution that aren't otherwise viewable
+// Define a classic struct
+#[derive(Debug)]
+struct MouseClick { _x: i64, _y: i64 }
+
+#[derive(Debug)]
+enum WebEvent { WELoad(bool), WEClick(MouseClick), WEKeys(String, char) }
+
 fn main() {
     vars();
+    types();
 }
 
 fn vars() {
@@ -36,6 +45,18 @@ fn vars() {
     // the old value is still available, but can't be used anymore
     println!("first_var (amongus) after mutation: {}", first_var); // prints 4
 
+    // tuples
+    // elements can't be added or removed from a tuple.
+    let tuple = (500, 6.4, 1);
+    println!("The value of tuple is: {:#?}", tuple); // prints (500, 6.4, 1)
+    // as you saw, in rust the simple {} can't be used to print a tuple, you have to use {:?} {:#?} instead
+    // the difference between them is that {:#?} prints the tuple in a more readable way (with line breaks and indentation)
+    // accessing tuple elements
+    let second_value = tuple.1; // tuple.1 is a float (this works different than I thought (I'm not gonna explain here because I don't want to sound stupid))
+    println!("The value of tuple.0 is: {}, and tuple.1 is: {}", tuple.0, second_value); // prints 500
+}
+
+fn types() {
     // booleans
     let is_bigger = 1 > 4; // type is bool
     println!("Is 1 > 4? {}", is_bigger);
@@ -63,4 +84,56 @@ fn vars() {
     let string_2: &str = "face";
     let string_3 = string_1.to_string() + string_2; // to_string coverts the given value to a String
     println!("{} is a {}{}", smiley_face, uppercase_s as char, string_3); // prints 😃 is a Smiley face (casting again otherwise it would print the number 83)
+    
+    // data collections
+    // structs
+    // those are types composed of other types (benefits: naming each field, so it's clear what the values mean)
+    // classic struct: fields with names and can be accessed by using dot notation (<struct>.<field>)
+    // tuple struct: fields without names and can be accessed by using dot notation (<struct>.<index>)
+    // unit-like struct: commonly used as markers (we'll see later it seems)
+    // defining the structs
+    // Classic struct with named fields
+    struct Student { name: String, level: u8, remote: bool }
+    // Tuple struct with data types only
+    struct Grades(char, char, char, char, f32);
+    // Unit struct
+    struct _Unit;
+    // instantiating the structs
+    let student = Student { name: String::from("John"), level: 5, remote: false };
+    let mark = Grades('A', 'A', 'A', 'A', 3.75);
+    // accessing the structs
+    println!("{} is in level {}, isRemote: {}", student.name, student.level, student.remote);
+    println!("{} {} {} {} {} is a {} student", mark.0, mark.1, mark.2, mark.3, mark.4, student.level);
+    // as you saw: String data that's stored inside another data structure, such as a struct or vector, must be converted
+    // from a string literal reference (&str) to a String type. To do the conversion, we use the standard String::from(&str) or .to_string() method.
+    
+    // enums (algebraic data types)
+    // enums are variants, and each variant can have different types and amounts of associated data
+    // defining the enum
+    enum _WebEvent {
+        // An enum variant can be like a unit struct without fields or data types
+        WELoad,
+        // An enum variant can be like a tuple struct with data types but no named fields
+        WEKeys(String, char),
+        // An enum variant can be like a classic struct with named fields and their data types
+        WEClick { x: i64, y: i64 }
+    }
+    // WELoad has no associated data type or data.
+    // WEKeys has two fields with data types String and char.
+    // WEMClick contains an anonymous struct with named fields x and y, and their data types (i64).
+    // Any function that uses a variant of the WebEvent enum must accept all the variants in the enum.
+    // Each variant in the enum isn't its own type.
+    // defining an enum with structs
+    // Define a classic struct
+    struct _MouseClick { x: i64, y: i64 }
+    // Redefine the enum variants to use the data from the new structs
+    // Update the page Load variant to have the boolean type
+    enum _WebEvent2 { WELoad(bool), WEClick(MouseClick), WEKeys(String, char) }
+    // instantiate an enum
+    let load = WebEvent::WELoad(true); // simple enum variant
+    let click = MouseClick { _x: 100, _y: 250 }; // instantiate a MouseClick struct and bind the coordinate values
+    let we_click = WebEvent::WEClick(click); // enum variant with a struct
+    let we_keys = WebEvent::WEKeys("Ctrl".to_string(), 'C'); // enum variant with a tuple struct
+    // print values
+    println!("\nWebEvent enum variants: \n{:?}\n{:?}\n{:?}\n", load, we_click, we_keys);
 }
