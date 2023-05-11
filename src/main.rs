9010d1d8cc07fed102e9f72fc35188dd0178f106
datsfilipe <datsfilipe.foss@proton.me>
2023-05-11 18:48:51 -0300

    feat: add car challenge

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+// challenge
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
+
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+enum Transmission {
+    // todo!("Fix enum definition so code compiles");
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
+    let car: Car = Car {
+        color,
+        transmission,
+        convertible,
+        mileage: 0,
+    };
+    car
+}
+
 // the #[derive(Debug)] syntax lets us see certain values during the code execution that aren't otherwise viewable
 // Define a classic struct
 #[derive(Debug)]
@@ -10,6 +37,13 @@ fn main() {
     vars();
     types();
     functions();
+
+    let mut car = car_factory(String::from("red"), Transmission::Manual, false);
+    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+    car = car_factory(String::from("silver"), Transmission::Automatic, true);
+    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+    car = car_factory(String::from("green"), Transmission::SemiAuto, false);
+    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
 }
 
 fn vars() {
